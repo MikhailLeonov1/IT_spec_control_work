82b2f0fe415638d2c9a299cecfdc88a30402d4d6
user name <you@example.com>
2023-02-14 17:09:50 +0300

    Раздел получения данных от пользователя

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, 
+// либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры,
+//  либо задать на старте выполнения алгоритма. При решении не рекомендуется 
+//  пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+
+
+Console.WriteLine("Введите колличество значений");
+int countArrayValue = int.Parse(Console.ReadLine());
+string[] startArray = new string[countArrayValue];
+string[] FillArray(string[] array)
+{
+    for (int i = 0; i < countArrayValue; i++)
+    {
+        Console.WriteLine($"Введите {i} значение:");
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
+
